f45d41afbe27c3c9bc9e8d117d0af869b548e9a1
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-27 21:56:57 +0200

    finish

--- src/ait/numbers/model/ExecutorGroupSum.java
+++ src/ait/numbers/model/ExecutorGroupSum.java
@@ -1,5 +1,14 @@
 package ait.numbers.model;
 
+import ait.numbers.task.OneGroupSum;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ExecutorGroupSum extends GroupSum{
     public ExecutorGroupSum(int[][] numberGroups) {
         super(numberGroups);
@@ -7,7 +16,45 @@ public class ExecutorGroupSum extends GroupSum{
 
     @Override
     public int computeSum() {
+        OneGroupSum[] tasks = new OneGroupSum[numberGroups.length];
+        int cpus = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(cpus * 2);
+
+        List<Future<OneGroupSum>> futures = new ArrayList<>();
+
+        for (int i = 0; i < numberGroups.length; i++) {
+            OneGroupSum task = new OneGroupSum(numberGroups[i]);
+            tasks[i] = task;
+            futures.add(executor.submit(() -> {
+                task.run();
+                return task;
+            }));
+        }
+
+//        for (Future<OneGroupSum> future : futures) {
+//            try {
+//                futures.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+        int totalSum = 0;
+        for (Future<OneGroupSum> future : futures) {
+            OneGroupSum task = null;
+            try {
+                task = future.get();
+                totalSum = totalSum + task.getSum();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return totalSum;
+
+
+
         // TODO ExecutorGroupSum
-        return 0;
+//        return 0;
     }
 }
--- src/ait/numbers/model/ThreadGroupSum.java
+++ src/ait/numbers/model/ThreadGroupSum.java
@@ -1,5 +1,7 @@
 package ait.numbers.model;
 
+import ait.numbers.task.OneGroupSum;
+
 public class ThreadGroupSum extends GroupSum {
     public ThreadGroupSum(int[][] numberGroups) {
         super(numberGroups);
@@ -7,7 +9,31 @@ public class ThreadGroupSum extends GroupSum {
 
     @Override
     public int computeSum() {
+        OneGroupSum[] tasks = new OneGroupSum[numberGroups.length];
+        Thread[] threads =new Thread[numberGroups.length];
+
+        for (int i = 0; i < numberGroups.length; i++) {
+            tasks[i] = new OneGroupSum(numberGroups[i]);
+            threads[i] = new Thread(tasks[i]);
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int totalSum = 0;
+        for (OneGroupSum task : tasks) {
+            totalSum = totalSum + task.getSum();
+        }
+
+        return totalSum;
+
         // TODO ThreadGroupSum
-        return 0;
+//        return 0;
     }
 }
